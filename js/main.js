bee69c889931f7fb749c888991a3ecedf3aeb192
jesherwood <jsherwood@simonsfoundation.org>
2022-09-09 16:53:33 -0400

    Added transitions to and height to the block elements.

--- js/main.js
+++ js/main.js
@@ -1,53 +1,49 @@
-// Light/dark theme switch
+// || THEME SWITCH ||
 const mainBody = document.getElementsByTagName('body')[0];
 
 function toggleMode() {
     mainBody.classList.toggle('dark');
 }
 
-// Slide item in from the right
+// || SLIDE TOGGLE IN FROM RIGHT ||
 const dayIcon = document.getElementsByClassName('day-toggle')[0];
 
 window.addEventListener('load', (slideFromRight) => {
     dayIcon.classList.toggle('live');
 });
 
+// || NAVIGATION AND MAIN CONTENT ||
+
 // open/close nav items & blocks
 const navItems = document.getElementsByClassName('navitem');
-const blocks = document.getElementsByClassName('block');
 const landingContent = document.getElementsByClassName('home')[0];
 const main = document.getElementsByTagName('main')[0];
+const auburnBlock = document.getElementsByClassName('block')[0];
+const deepSpaceSparkleBlock = document.getElementsByClassName('block')[1];
+const indianYellowBlock = document.getElementsByClassName('block')[2];
 
 for (let i = 0; i < navItems.length; i++) {
     navItems[i].addEventListener('click', function() {
         if (!this.classList.contains('selected')) {
             for (let i = 0; i < navItems.length; i++) {
-              navItems[i].classList.remove('selected');
+                navItems[i].classList.remove('selected');
+                auburnBlock.classList.remove('expand');
+                deepSpaceSparkleBlock.classList.remove('expand');
+                indianYellowBlock.classList.remove('expand');
             }
             this.classList.toggle('selected');
+            if (this.classList.contains('auburn')) {
+                auburnBlock.classList.add('expand');
+            } else if (this.classList.contains('deep-space-sparkle')) {
+                deepSpaceSparkleBlock.classList.add('expand');
+            } else {
+                indianYellowBlock.classList.add('expand');
+            }
         }
         landingContent.classList.add('closed');
         main.style.paddingTop = '0.5rem';
     });
 }
-
-// for (let j = 0; j < blocks.length; j++) {
-//     for (let k = 0; k < navItems.length; k++) {
-//         if (navItems[k].classList.contains('selected')) {
-//             let openItem = navItems[k];
-//             if (openItem) {
-//                 console.log('Hey');
-//                 if (openItem.classList.contains('auburn')) {
-//                     if (blocks[j].classList.contains('auburn')) {
-//                         blocks[j].style.display = 'block';
-//                     } else {
-//                         blocks[j].style.display = 'none';
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
 
 // close nav items when on home
 landingContent.addEventListener('click', function() {
@@ -56,9 +52,26 @@ landingContent.addEventListener('click', function() {
         landingContent.classList.remove('closed');
         main.style.paddingTop = '1rem';
     }
+    auburnBlock.classList.remove('expand');
+    deepSpaceSparkleBlock.classList.remove('expand');
+    indianYellowBlock.classList.remove('expand');
 });
 
-// Date
+//close item when header is clicked
+// const closer = document.getElementsByClassName('closer');
+// for (let i = 0; i < closer.length; i++) {
+//     closer[i].addEventListener('click', function() {
+//         let blocks = document.getElementsByClassName('block');
+//         blocks[0].classList.remove('expand');
+//         navItems[0].classList.remove('selected')
+//         for (let j = 0; j < blocks; j++) {
+//             blocks[j].classList.remove('expand');
+//             console.log('FUCK');
+//         }
+//     });
+// }
+
+// || DATE IN FOOTER ||
 let date = new Date();
 let year = date.getFullYear();
 document.getElementsByClassName('date')[0].innerHTML = year;
