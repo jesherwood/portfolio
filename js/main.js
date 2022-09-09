ad0bf567ff6d082fe6440bbb5b70a848d9ebde36
jesherwood <jsherwood@simonsfoundation.org>
2022-09-09 13:58:20 -0400

    Added javascript to start building out open/close for nav and content.

--- js/main.js
+++ js/main.js
@@ -1,47 +1,62 @@
-// Nav item expander
-const navItems = document.getElementsByClassName('navitem');
-
-for (let i = 0; i < navItems.length; i++) {
-  navItems[i].addEventListener('click', function() {
-        const navItem = document.getElementsByClassName('navitem');
-        let setClasses = !this.classList.contains('expand');
-        for (let it of navItem) {
-            console.log(it);
-            it.classList.remove('expand');
-        }
-        if (setClasses) {
-            this.classList.toggle('expand');
-        }
-        let hasExpand = this.classList.contains('expand');
-        if (hasExpand) {
-            let blocks = document.getElementsByClassName('block');
-            for (let i = 0; i < blocks.length; i++) {
-                const block = blocks[i];
-                console.log('HEY');
-                if (block.classList.contains('show')) {
-                    block.classList.remove('show')
-                } else {
-                    block.classList.toggle('show');
-                }
-                // let setStyles = !block.classList.contains('show');
-                // for (let thing of block) {
-                //     console.log(thing);
-                //     thing.classList.remove('show');
-                // }
-                // if (setStyles) {
-                //     block.classList.toggle('show');
-                // }
-            }
-        }
-    });
-}
-
 // Light/dark theme switch
 const mainBody = document.getElementsByTagName('body')[0];
 
 function toggleMode() {
     mainBody.classList.toggle('dark');
 }
+
+// Slide item in from the right
+const dayIcon = document.getElementsByClassName('day-toggle')[0];
+
+window.addEventListener('load', (slideFromRight) => {
+    dayIcon.classList.toggle('live');
+});
+
+// open/close nav items & blocks
+const navItems = document.getElementsByClassName('navitem');
+const blocks = document.getElementsByClassName('block');
+const landingContent = document.getElementsByClassName('home')[0];
+const main = document.getElementsByTagName('main')[0];
+
+for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', function() {
+        if (!this.classList.contains('selected')) {
+            for (let i = 0; i < navItems.length; i++) {
+              navItems[i].classList.remove('selected');
+            }
+            this.classList.toggle('selected');
+        }
+        landingContent.classList.add('closed');
+        main.style.paddingTop = '0.5rem';
+    });
+}
+
+// for (let j = 0; j < blocks.length; j++) {
+//     for (let k = 0; k < navItems.length; k++) {
+//         if (navItems[k].classList.contains('selected')) {
+//             let openItem = navItems[k];
+//             if (openItem) {
+//                 console.log('Hey');
+//                 if (openItem.classList.contains('auburn')) {
+//                     if (blocks[j].classList.contains('auburn')) {
+//                         blocks[j].style.display = 'block';
+//                     } else {
+//                         blocks[j].style.display = 'none';
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// close nav items when on home
+landingContent.addEventListener('click', function() {
+    for (let i = 0; i < navItems.length; i++) {
+        navItems[i].classList.remove('selected');
+        landingContent.classList.remove('closed');
+        main.style.paddingTop = '1rem';
+    }
+});
 
 // Date
 let date = new Date();
