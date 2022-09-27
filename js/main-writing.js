7fee858c1529037ed883143a456949a572eb9cdf
jesherwood <jsherwood@simonsfoundation.org>
2022-09-27 15:24:12 -0400

    Added functions to change hash and color on scroll and to maintain the same functionality on nav click.

--- js/main-writing.js
+++ js/main-writing.js
@@ -10,59 +10,57 @@ window.addEventListener('load', (growContent) => {
     }, delayInMilliseconds);
 });
 
-// change section hash on scroll
+// change section hash & color on scroll
 
-//...have to fix: it reads the place on the page and overpowers the nav links...
 const sections = document.getElementsByTagName('section');
+let len = sections.length;
 
 function updateFragId() {
-    let len = sections.length;
     for (let i = 0; i < len; i++) {
         let id = sections[i].id;
         let rect = sections[i].getBoundingClientRect().y;
         let pageData = {id:id, rect:rect};
-        let url = window.location.toString();
-        console.log(url);
         if (pageData.rect > -100 && pageData.rect < 100) {
             if (pageData.id !== location.hash) {
-                  fragmentId = pageData.id;
-                  var uri = window.location.toString();
-                  console.log(fragmentId);
-                  window.location.hash = fragmentId;
+                fragmentId = pageData.id;
+                window.location.hash = fragmentId;
+                if (new URL(document.URL).hash === '#work') {
+                    document.documentElement.style.setProperty('--color', 'rgb(165, 36, 34)');
+                } else if (new URL(document.URL).hash === '#contact') {
+                    document.documentElement.style.setProperty('--color', 'rgb(0, 110, 144)');
+                } else {
+                    document.documentElement.style.setProperty('--color', 'rgb(0, 0, 0)');
+                    const noHash = window.location.href.split('#')[0];
+                    window.history.pushState('', 'Home', noHash);
+                }
             }
         }
     }
 }
 
-// change site color variable on menu item
-let navItem = document.getElementsByClassName('nav-item');
-
-navItem[0].addEventListener('click', (colorChange) => {
-    document.documentElement.style.setProperty('--color', 'rgb(0, 0, 0)');
-});
-
-navItem[1].addEventListener('click', (colorChange) => {
-    document.documentElement.style.setProperty('--color', 'rgb(165, 36, 34)');
-});
-
-navItem[2].addEventListener('click', (colorChange) => {
-    document.documentElement.style.setProperty('--color', 'rgb(0, 110, 144)');
-});
-
-if (new URL(document.URL).hash === '#work') {
-    document.documentElement.style.setProperty('--color', 'rgb(165, 36, 34)');
-} else if (new URL(document.URL).hash === '#contact') {
-    document.documentElement.style.setProperty('--color', 'rgb(0, 110, 144)');
-};
-     // this fixes bug where the entered url of a hash wont drop to the right section
-window.onload = function() {
-    if (window.location.hash) {
-        elmnt = document.getElementById(window.location.hash.substring(1));
-            elmnt.scrollIntoView(true);
-    }
-};
-
 window.addEventListener('scroll', updateFragId);
+
+
+// turn off above ^^ scroll event when a nav item is clicked and change color
+function navClick() {
+    window.removeEventListener('scroll', updateFragId);
+
+    let navItem = document.getElementsByClassName('nav-item');
+
+    navItem[0].addEventListener('click', (colorChange) => {
+        document.documentElement.style.setProperty('--color', 'rgb(0, 0, 0)');
+    });
+    navItem[1].addEventListener('click', (colorChange) => {
+        document.documentElement.style.setProperty('--color', 'rgb(165, 36, 34)');
+    });
+    navItem[2].addEventListener('click', (colorChange) => {
+        document.documentElement.style.setProperty('--color', 'rgb(0, 110, 144)');
+    });
+
+    setTimeout(() => {
+        window.addEventListener('scroll', updateFragId);
+    }, 500);
+}
 
 // date in footer
 let date = new Date();
